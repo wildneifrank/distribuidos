94cc61cc87b885a43122666066af77f6b7a9e109
cmatheuslf <cmatheuslf@alu.ufc.br>
2023-10-04 23:40:08 -0300

    ajeitando gateway

--- projeto1-2/MulticastReceiver.py
+++ projeto1-2/MulticastReceiver.py
@@ -1,28 +1,57 @@
 import socket
+import netifaces
+import subprocess
+from protoBuff import MulticastMessage_pb2 as MulticastMessage
 
 class MulticastReceiver:
-    def __init__(self, multicast_group, multicast_port):
+    def __init__(self, multicast_group, multicast_port, interface_ip):
         self.multicast_group = multicast_group
         self.multicast_port = multicast_port
+        self.interface_ip = interface_ip
 
-        # Crie um socket UDP multicast
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-        # Junte-se ao grupo multicast em todas as interfaces
-        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(multicast_group) + socket.inet_aton('0.0.0.0'))
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.interface_ip, self.multicast_port))
 
-        # Associe o socket à porta do grupo multicast
-        self.sock.bind(('0.0.0.0', multicast_port))
+        mreq = socket.inet_aton(self.multicast_group) + socket.inet_aton(self.interface_ip)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-    def receive_messages(self):
+    def receive_multicast_messages(self):
         while True:
-            # Receba mensagens do grupo multicast
-            data, address = self.sock.recvfrom(1024)
-            print(f"Recebido de {address}: {data.decode('utf-8')}")
+            data, _ = self.sock.recvfrom(1024)
+
+            # Desserializar a mensagem usando o mesmo módulo gerado
+            message = MulticastMessage.MulticastMessage()
+            message.ParseFromString(data)
+
+            print(f"De: {message.sender}\nMensagem: {message.content}\n")
+
+            return message.sender
+
+    def close(self):
+        self.sock.close()
+
+def get_active_interface_ip(interface_name):
+    try:
+        output = subprocess.check_output(["ifconfig", interface_name]).decode("utf-8")
+        lines = output.split("\n")
+        for line in lines:
+            if "inet " in line:
+                parts = line.split()
+                return parts[1]  # O endereço IPv4 está na segunda parte
+    except Exception as e:
+        print(f"Erro ao obter a interface IP: {e}")
+        return None
 
 if __name__ == "__main__":
     multicast_group = '224.0.0.1'
     multicast_port = 5000
+    interface_ip = get_active_interface_ip("wifi0")
 
-    receiver = MulticastReceiver(multicast_group, multicast_port)
-    receiver.receive_messages()
+    receiver = MulticastReceiver(multicast_group, multicast_port, interface_ip)
+    sender_ip = receiver.receive_multicast_messages()
+
+    # Para fechar os sockets quando terminar, você pode chamar os métodos 'close':
+    receiver.close()
+        
--- projeto1-2/gateway.py
+++ projeto1-2/gateway.py
@@ -1,16 +1,15 @@
 import socket
-from protoBuff import MulticastMessage
+import netifaces
+import subprocess
+from protoBuff import MulticastMessage_pb2 as MulticastMessage
 
 class MulticastSender:
-    def __init__(self, multicast_group, multicast_port):
+    def __init__(self, multicast_group, multicast_port, interface_ip):
         self.multicast_group = multicast_group
         self.multicast_port = multicast_port
-
-        interfaces = socket.getaddrinfo(socket.gethostname(), None)
-        self.interface_ip = interfaces[3][4][0]
+        self.interface_ip = interface_ip
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(multicast_group) + socket.inet_aton(self.interface_ip))
 
     def send_multicast_message(self, sender, content):
@@ -25,41 +24,23 @@ class MulticastSender:
     def close(self):
         self.sock.close()
 
-class MulticastReceiver:
-    def __init__(self, multicast_group, multicast_port):
-        self.multicast_group = multicast_group
-        self.multicast_port = multicast_port
-
-        interfaces = socket.getaddrinfo(socket.gethostname(), None)
-        self.interface_ip = interfaces[3][4][0]
-
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((self.interface_ip, self.multicast_port))
-
-        mreq = socket.inet_aton(self.multicast_group) + socket.inet_aton(self.interface_ip)
-        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
-    def receive_multicast_messages(self):
-        while True:
-            data, _ = self.sock.recvfrom(1024)
-
-            # Desserializar a mensagem usando o mesmo módulo gerado
-            message = MulticastMessage.MulticastMessage()
-            message.ParseFromString(data)
-
-            print(f"De: {message.sender}\nMensagem: {message.content}\n")
-
-    def close(self):
-        self.sock.close()
+def get_active_interface_ip(interface_name):
+    try:
+        output = subprocess.check_output(["ifconfig", interface_name]).decode("utf-8")
+        lines = output.split("\n")
+        for line in lines:
+            if "inet " in line:
+                parts = line.split()
+                return parts[1]  # O endereço IPv4 está na segunda parte
+    except Exception as e:
+        print(f"Erro ao obter a interface IP: {e}")
+        return None
 
 if __name__ == "__main__":
     multicast_group = '224.0.0.1'
     multicast_port = 5000
+    interface_ip = get_active_interface_ip("wifi0")
 
-    sender = MulticastSender(multicast_group, multicast_port)
-    sender.send_multicast_message('Enviador', 'Esta é uma mensagem de teste')
-
-    receiver = MulticastReceiver(multicast_group, multicast_port)
-    receiver.receive_multicast_messages()
+    sender = MulticastSender(multicast_group, multicast_port, interface_ip)
+    sender.send_multicast_message(interface_ip, 'Esta é uma mensagem de teste do Matheus')
+    sender.close()
