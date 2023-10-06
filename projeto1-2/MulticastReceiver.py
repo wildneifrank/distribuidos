af0a02d7a99d969ccdac25ce5b52a19e2eed462d
Bruna <brunastefanie@alu.ufc.br>
2023-10-06 13:26:17 -0300

    mudanças lampada

---
+++ projeto1-2/MulticastReceiver.py
@@ -0,0 +1,56 @@
+import socket
+import netifaces
+import subprocess
+from protoBuff import MulticastMessage_pb2 as MulticastMessage
+
+class MulticastReceiver:
+    def __init__(self, multicast_group, multicast_port, interface_ip):
+        self.multicast_group = multicast_group
+        self.multicast_port = multicast_port
+        self.interface_ip = interface_ip
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.interface_ip, self.multicast_port))
+
+        mreq = socket.inet_aton(self.multicast_group) + socket.inet_aton(self.interface_ip)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    def receive_multicast_messages(self):
+        while True:
+            data, _ = self.sock.recvfrom(1024)
+
+            # Desserializar a mensagem usando o mesmo módulo gerado
+            message = MulticastMessage.MulticastMessage()
+            message.ParseFromString(data)
+
+            print(f"De: {message.sender}\nMensagem: {message.content}\n")
+
+            return message.sender
+
+    def close(self):
+        self.sock.close()
+
+def get_active_interface_ip(interface_name):
+    try:
+        output = subprocess.check_output(["ifconfig", interface_name]).decode("utf-8")
+        lines = output.split("\n")
+        for line in lines:
+            if "inet " in line:
+                parts = line.split()
+                return parts[1]  # O endereço IPv4 está na segunda parte
+    except Exception as e:
+        print(f"Erro ao obter a interface IP: {e}")
+        return None
+
+if __name__ == "__main__":
+    multicast_group = '224.0.0.1'
+    multicast_port = 5000
+    interface_ip = get_active_interface_ip("eno1")
+
+    receiver = MulticastReceiver(multicast_group, multicast_port, interface_ip)
+    sender_ip = receiver.receive_multicast_messages()
+
+    # Para fechar os sockets quando terminar, você pode chamar os métodos 'close':
+    receiver.close()
---
+++ projeto1-2/gateway.py
@@ -0,0 +1,188 @@
+import socket
+import struct
+from protoBuff import MulticastMessage_pb2 as mumes
+from protoBuff import SensorMessage_pb2 as senmes
+import threading
+import MulticastReceiver as mrcv
+
+def multicast_sender():
+    # Configurações do multicast
+    multicast_group = '224.0.0.1'
+    multicast_port = 5000
+
+    # Crie um socket UDP para multicast
+    multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    # Configurar o socket para permitir que outros programas usem a mesma porta
+    multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Junte-se ao grupo multicast
+    multicast_socket.bind(('', multicast_port))
+    mreq = struct.pack('4sL', socket.inet_aton(multicast_group), socket.INADDR_ANY)
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    print(f"Escutando mensagens do grupo {multicast_group}:{multicast_port}")
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    interface_ip = s.getsockname()[0]
+    s.close()
+
+    try:
+        message = mumes.MulticastMessage()
+        message.sender = interface_ip
+        message.type = "Este dispositivo está online"
+
+        # Serialize a mensagem protobuf em bytes
+        message_bytes = message.SerializeToString()
+
+            # Envie a mensagem serializada como um pacote UDP multicast
+        multicast_socket.sendto(message_bytes, (multicast_group, multicast_port))
+
+        
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Espere por um curto período antes de enviar novamente (pode ajustar isso conforme necessário)
+            
+        multicast_socket.close()
+def obj_comunication(conn):
+    try:
+        data = conn.recv(1024)  # Tamanho máximo da mensagem é 1024 bytes
+        if data:
+            # Parse da mensagem protobuf
+            message = mumes.MulticastMessage()
+            message.ParseFromString(data)
+            sender = message.sender
+            type = message.type
+            print(f"Recebido de {sender}: {type}")
+            #cliente
+            if(type == '0'):
+                print('user')
+            #sensor
+            elif(type == '1'):
+                print('sensor')
+                sensor_thread = threading.Thread(target=sensor_handle, args=(sender, conn,))
+                sensor_thread.start()
+
+            #atuador
+            elif(type == '2'):
+                print('atuador')
+            #ar-condicionado
+            elif(type=='3'):
+                print('ar-condicionado')
+        print('chegou')
+    except:
+        print('close')
+        conn.close()
+        obj_sock.remove(conn)
+        
+    
+
+    
+
+############################# tratamentos de objetos #####################################
+
+#Sensor
+def sensor_handle(sender, conn):
+    objetos[sender] = None
+
+    try:
+        while True:
+            data = conn.recv(1024)
+            message = senmes.SensorMessage()
+            message.ParseFromString(data)
+            objetos[sender] = message.valor
+            print(f"Valor é: {objetos}")
+    
+    except:        
+        obj_sock.remove(conn)
+        conn.close()
+        
+    return 0
+
+#Atuador
+# def atuador_handle(sender):
+#     server_ip = '192.168.1.245'
+#     server_port = 6000
+#     # Crie um socket TCP
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#     # Vincular o socket ao endereço e porta do servidor
+#     sock.bind((server_ip, server_port))
+
+#     # Espere por conexões de clientes
+#     sock.listen(1)
+
+#     print(f"Servidor TCP escutando em {server_ip}:{server_port}")
+#     objetos[sender] = None
+
+#     while True:
+#         conn, addr = sock.accept()
+#         data = senmes.SensorMessage()
+#         data.ParseFromString(conn.recv(1024))
+#         objetos[sender] = data.valor
+#         print(f"Conexão estabelecida com {objetos}")
+        
+#     return 0
+
+#Ar-condicionado
+# def ac_handle(sender):
+#     server_ip = '192.168.1.245'
+#     server_port = 6000
+#     # Crie um socket TCP
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#     # Vincular o socket ao endereço e porta do servidor
+#     sock.bind((server_ip, server_port))
+
+#     # Espere por conexões de clientes
+#     sock.listen(1)
+
+#     print(f"Servidor TCP escutando em {server_ip}:{server_port}")
+#     objetos[sender] = None
+
+#     while True:
+#         conn, addr = sock.accept()
+#         data = senmes.SensorMessage()
+#         data.ParseFromString(conn.recv(1024))
+#         objetos[sender] = data.valor
+#         print(f"Conexão estabelecida com {objetos}")
+        
+#     return 0
+
+
+
+if __name__ == "__main__":
+    # Crie uma thread para executar a função multicast_sender
+    multicast_sender()
+
+    objetos = {}
+    obj_sock = []
+    server_ip = '192.168.1.245'
+    server_port = 8002
+    # Crie um socket TCP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Vincular o socket ao endereço e porta do servidor
+    sock.bind((server_ip, server_port))
+
+    # Espere por conexões de clientes
+    sock.listen(5)
+
+    
+
+    while(True):
+        conn, addr = sock.accept()
+        print(f"Escutando mensagens de {addr}")
+
+        obj_sock.append(conn)
+
+
+        obj_thread = threading.Thread(target=obj_comunication, args=(conn,))
+
+        # Inicie a thread
+        obj_thread.start()
+        print('foi')
+
+    
