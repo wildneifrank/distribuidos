eebdd30717865855219b6660d6062893aae90219
Bruna <brunastefanie@alu.ufc.br>
2023-10-07 00:08:59 -0300

    proto e lamapada.py

--- projeto1-2/lampada.py
+++ projeto1-2/lampada.py
@@ -19,39 +19,39 @@ class Lampada:
         return lampada_pb2.LampadaStatus(ligada=self.status)
 
 def main():
-    # Defina o IP e o tipo da lâmpada
-    lampada_ip = "127.0.0.1"  # Substitua pelo IP da lâmpada
+    
+    lampada_ip = "127.0.0.1"  
     lampada_tipo = "lampada"
 
-    # Crie uma instância da lâmpada
+    
     lampada = Lampada(lampada_ip, lampada_tipo)
 
-    # Estabeleça uma conexão TCP com o gateway
-    gateway_ip = "127.0.0.1"  # Substitua pelo IP do gateway
-    gateway_port = 8080  # Substitua pela porta do gateway
+    
+    gateway_ip = "127.0.0.1"  
+    gateway_port = 8080  
 
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((gateway_ip, gateway_port))
 
             while True:
-                # Aguarde uma mensagem do gateway
+                ## Aguarde uma mensagem do gateway
                 data = s.recv(1024)
 
                 if not data:
-                    break  # Encerre o loop se a conexão for encerrada pelo gateway
+                    break  
 
-                # Interpretar a mensagem do gateway
+                ## Interpretar a mensagem do gateway
                 control_msg = lampada_pb2.LampadaControl()
                 control_msg.ParseFromString(data)
 
-                # Mudar o status da lâmpada de acordo com a mensagem do gateway
+                
                 if control_msg.control:
                     lampada.ligar()
                 else:
                     lampada.desligar()
 
-                # Responder com o status atual
+                
                 status_msg = lampada.get_status().SerializeToString()
                 s.sendall(status_msg)
 
