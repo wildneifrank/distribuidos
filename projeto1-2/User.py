a2bcf8c7f8653d92a66d5759a2df104d1b4ed11d
cmatheuslf <cmatheuslf@alu.ufc.br>
2023-10-08 13:26:55 -0300

    interface feita e lphandle em confecção 1

--- projeto1-2/User.py
+++ projeto1-2/User.py
@@ -1,49 +1,55 @@
-class SensorTemp:
-    def __init__(self):
-        self.temperatura = None
-class ArCon:
-    def __init__(self):
-        self.temperatura = None
-        self.status = 'Desligado'
-class Lampada:
-    def __init__(self, ip, port):
-        self.ip = ''
-        self.port = ''
-import gateway as gtw
-
-
-senTemp = SensorTemp()
-arcon = ArCon()
-lamp = Lampada()
-
+import true_gateway as gtw
 
 def init_client(acao):
     print('Bem vindo ao Alexo Rabbit')
     
     while(True):
         print('Lista de ações:')
-        for i in range(len(acoes)):
-            print(i+1, '-', acoes[i])
+        for i in range(len(obj)):
+            print(i+1, '-', obj[i])
         acao = input('Digite o número da ação que deseja tomar?\n')
         if acao == '1':
-            print('A tempeatura do sensor é:', senTemp.temperatura)
+            print('Lista de ações:')
+            print('1 - Saber a temperatura do sensor')
+            act = input('Digite o número da ação que deseja tomar:')
+            # if act == '1':
+            #     gtw.sensor_handle(act)
         elif acao == '2':
-            print('O status da lampada é', lamp.status)
+            print('Lista de ações:')
+            for i in range(len(acoes_lp)):
+                print(i+1, '-', acoes_lp[i])
+            act = input('Digite o número da ação que deseja tomar:')
+            if act == '1':
+                gtw.lp_handle(act)
+            elif act == '2':
+                gtw.lp_handle(act)
+            elif act == '3':
+                gtw.lp_handle(act)
         elif acao == '3':
-            print('')
-        elif acao == '4':
-            print('')
-        elif acao == '5':
-            print('')
-        elif acao == '6':
-            print('')
-        elif acao == '7':
-            print('')
+            print('Lista de ações:')
+            for i in range(len(acoes_ac)):
+                print(i+1, '-', acoes_ac[i])
+            act = input('Digite o número da ação que deseja tomar:')
+            # if act == '1':
+            #     gtw.ac_handle(act)
+            # elif act == '2':
+            #     gtw.ac_handle(act)
+            # elif act == '3':
+            #     gtw.ac_handle(act)
 
-acoes=[ 
-    'Saber a temperatura do sensor',
+obj = [
+    'Sensor de Temperatura',
+    'Lampada',
+    'Ar condicionado'
+]
+acoes_lp=[ 
+    
     'Status da lampada',
-    'Ligar/Desligar a lampada',
+    'Ligar',
+    'Desligar a lampada'
+]
+acoes_ac = [
+    'Ligar'
     'Temperatura do ar-condicionado',
     'Status do ar-condicionado',
     'Aumentar temperatura',
--- projeto1-2/true_gateway.py
+++ projeto1-2/true_gateway.py
@@ -3,7 +3,38 @@ import MulticastReceiver as mrcv
 import MulticastSender as msnd
 import threading
 import csv
+import User as usr
+import pandas as pd
+from protoBuff import lampada_pb2 as lames
 
+def tcpComunicationLamp(ip, port, op):
+    # Configurações do servidor
+    host = ip  # Endereço IP ou nome de host do servidor
+    porta = port       # Porta do servidor
+
+    # Mensagem que você deseja enviar
+    lpctrl = lames.LampadaControl()
+    lpctrl.control = op
+    msg_srl = lpctrl.SerializeToString
+
+    # Crie um socket TCP
+    cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Conecte-se ao servidor
+    cliente_socket.connect((host, porta))
+
+    # Envie a mensagem para o servidor
+    cliente_socket.send(msg_srl)
+
+    # Aguarde a resposta do servidor (opcional)
+    lpstatus = lames.Lampada()
+
+    data = cliente_socket.recv(1024)
+    lpstatus.ParseFromString(data)
+    print(f'Status da lampada: {lpstatus.status}')
+
+    # Feche o socket do cliente
+    cliente_socket.close()
 
 
 def multiRcv():
@@ -14,7 +45,7 @@ def multiRcv():
         nome_arquivo = 'dados.csv'
 
         # Abra o arquivo CSV para escrita
-        with open(nome_arquivo, 'w', newline='') as arquivo_csv:
+        with open(nome_arquivo, 'a', newline='') as arquivo_csv:
             # Crie um objeto escritor CSV
             escritor_csv = csv.writer(arquivo_csv)
 
@@ -23,15 +54,42 @@ def multiRcv():
 
         print(f"A tupla {disp} foi escrita no arquivo CSV '{nome_arquivo}'.")
 
+def lp_handle(acao):
+    caminho_arquivo = 'dados.csv'
+    df = pd.read_csv(caminho_arquivo)
+    if '1' in df['type'].values:
+        index = df.index[df['type'] == '2']
+        line = df.iloc[index]
+        tcpComunicationLamp(line[0],line[1], acao)
+
+    else:
+        print(f'Não existe uma lampada conectada.')
+
+    
 
 
 
 
 if __name__ == "__main__":
+    #envia o multicast
     msnd.multicast_sender()
 
+    #nomeia labels do arquivo csv
+    nome_arquivo = 'dados.csv'
+    labels = ('ip', 'port', 'type')
+    with open(nome_arquivo, 'a', newline='') as arquivo_csv:
+            # Crie um objeto escritor CSV
+            escritor_csv = csv.writer(arquivo_csv)
+
+            # Escreva a tupla no arquivo CSV
+            escritor_csv.writerow(labels)
+
+    # thread para receber infos dos dispositivos
     mrcv_thread = threading.Thread(target=multiRcv)
     mrcv_thread.start()
+    
+    #receber comandos do usuario
+    usr.init_client()
 
 
 
